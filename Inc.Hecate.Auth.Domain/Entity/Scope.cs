39823c4c7f397b3dd1ec363dda61c50d1cfbebf8
linh-00 <alinnelauren@hotmail.com>
2025-04-24 12:07:52 -0300

    Fix.

--- Inc.Hecate.Auth.Domain/Entity/Scope.cs
+++ Inc.Hecate.Auth.Domain/Entity/Scope.cs
@@ -12,17 +12,35 @@ namespace Inc.Hecate.Auth.Domain.Entity
         public Guid Id { get; private set; }
         public string Name { get; private set; }
         public string Description { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public string CreatedBy { get; private set; }
+        public DateTime? UpdatedAt { get; private set; }
+        public string? UpdatedBy { get; private set; }
 
-        public Scope(Guid id, string name, string description): base(id)
+        public Scope(Guid id, string name, string description, DateTime createdAt, string createdBy, DateTime? updatedAt, string? updatedBy) : base(id)
         {
             Name = name;
             Description = description;
+            CreatedAt = createdAt;
+            CreatedBy = createdBy;
+            UpdatedAt = updatedAt;
+            UpdatedBy = updatedBy;
         }
 
-        public Scope(string name, string description)
+        public Scope(string name, string description, string createdBy)
         {
             Name = name;
             Description = description;
+            CreatedAt = DateTime.Now;
+            CreatedBy = createdBy;            
+        }
+        public void Update(string name, string description, string updatedBy)
+        {
+            Name = name;
+            Description = description;
+            UpdatedAt = DateTime.Now;
+            UpdatedAt = DateTime.Now;
+            UpdatedBy = updatedBy;            
         }
     }
 }
