3b47f459be14caef114846c3778b7e8cef8071b5
linh-00 <alinnelauren@hotmail.com>
2025-04-28 17:05:30 -0300

    Fix

--- Inc.Hecate.Auth.DAL/Repository/UserRepository.cs
+++ Inc.Hecate.Auth.DAL/Repository/UserRepository.cs
@@ -1,21 +1,41 @@
 ﻿using Azure.Core;
+using Inc.Hecate.Auth.DAL.Context;
+using Inc.Hecate.Auth.DAL.Models;
 using Inc.Hecate.Auth.Domain.Entity;
 using Inc.Hecate.Auth.Domain.Interface;
+using Microsoft.Identity.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserEntity = Inc.Hecate.Auth.Domain.Entity.User;
 
 namespace Inc.Hecate.Auth.DAL.Repository
 {
     public class UserRepository : IUserRepository
     {
-        public User GetUserByEmailAndPassword(string password, string email)
+        public readonly dbHecateContext _Context;
+        public UserRepository(dbHecateContext context)
         {
-            throw new Exception();
-
-            //return new User(,2,"","");
+            _Context = context;
         }
+
+        public async Task<UserEntity> CreateUser(UserEntity request)
+        {
+            Models.User newUser = new Models.User
+            {
+                ACCOUNT_ID = request.AccountId,
+                EMAIL = request.Email,
+                PASSWORD = request.Password,
+                CREATED_AT = request.CreatedAt,
+                CREATED_BY = request.UpdatedBy
+            };
+            _Context.Users.Add(newUser);
+            await _Context.SaveChangesAsync();
+            request.SetId(newUser.ID);
+            return request;
+        }
+
     }
 }
--- Inc.Hecate.Auth.Domain/Interface/IUserRepository.cs
+++ Inc.Hecate.Auth.Domain/Interface/IUserRepository.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserEntity = Inc.Hecate.Auth.Domain.Entity.User;
 
 namespace Inc.Hecate.Auth.Domain.Interface
 {
     public interface IUserRepository
     {
-        User GetUserByEmailAndPassword(string email, string password);
+        Task<UserEntity> CreateUser(UserEntity request);
     }
 }
